f416322c7a8de2add97d3d822f3f2214764541b2
Shahin Sorkh <sorkh.shahin@protonmail.com>
2023-01-02 16:08:55 +0330

    update eslint and install sqlstring

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   env: {
     commonjs: true,
-    es2020: true,
+    es2021: true,
     node: true
   },
   extends: [
     'standard'
   ],
   parserOptions: {
-    ecmaVersion: 11
+    ecmaVersion: 'latest'
   },
   rules: {
   }
